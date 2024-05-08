db80d331ab2f1506e59007c796d4de66065eebd0
fabiiianac15 <fjacostaa@ufpso.edu.co>
2024-05-08 12:46:53 -0500

    Vue y otra vaina

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+
+let number = ref(0)
+let time = ref(0)
+let interval
+
+function add(){
+    if(number.value == 0){
+        interval = setInterval(() => {
+            time.value++
+        }, 1000)
+    }
+
+    number.value++
+
+    if(number.value == 100){
+        clearInterval(interval)
+    }
+}
+</script>
+
+<template>
+    <div v-show="number > 0" class=" float-right mr-8 bg-red-200 rounded inline-block py-1 px-2 ">
+        Tiempo : {{ time }}
+    </div>
+<div @click="add" class=" hover: cursor-pointer select-none bg-green-600 text-white py-1 px-2 rounded w-12 text-center mt-10 mx-auto">
+    {{ number}}
+</div>
+<div v-if="number == 0" class=" bg-orange-300 text-center w-52 mx-auto mt-8 rounded py-1 px-2 ">
+    Clikear o miedo? 🐱‍👓
+</div>
+<div v-else="number == 0" class=" bg-green-500 text-center w-52 mx-auto mt-8 rounded py-1 px-2 ">
+    Vamos Careverga 😡
+</div>
+
+</template>
